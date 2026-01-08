7f707e9fc3bcdbf4f2222adaf312796c5c5d5b76
Lukas Forst <forstlukas245@student.voskh.cz>
2026-01-08 15:21:04 +0100

    prvni commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var morseAlphabet = [
+    ".-", //A
+    "-...", //B
+    "-.-.", //C
+    "-..", //D
+    ".", //E
+    "..-.", //F
+    "--.", //G
+    "....", //H
+    "..", //I
+    ".---", //J
+    "-.-", //K
+    ".-..", //L
+    "--", //M
+    "-.", //N
+    "---", //O
+    ".--.", //P
+    "--.-", //Q
+    ".-.", //R
+    "...", //S
+    "-", //T
+    "..-", //U
+    "...-", //V
+    ".--", //W
+    "-..-", //X
+    "-.--", //Y
+    "--.." //Z
+];
+function textToMorse(vstup) {
+    var vystup = "";
+    vstup = vstup.toUpperCase();
+    for (var i = 0; i < vstup.length; i++) {
+        var kod = vstup.charCodeAt(i);
+        if (kod >= 65 && kod <= 90) { // A-Z
+            vystup += morseAlphabet[kod - 65] + " ";
+        }
+        else if (kod === 32) { // space
+            vystup += "/ ";
+        }
+    }
+    return vystup;
+}
+function morseToText(vstup) {
+    var vystup = "";
+    var morseChars = vstup.split(" ");
+    for (var i = 0; i < morseChars.length; i++) {
+        if (morseChars[i] === "/") {
+            vystup += " ";
+        }
+        else {
+            var index = morseAlphabet.indexOf(morseChars[i]);
+            if (index !== -1) {
+                vystup += String.fromCharCode(index + 65);
+            }
+        }
+    }
+    return vystup;
+}
+// Test cases
+console.log(textToMorse("HELLO WORLD"));
+console.log(textToMorse("MMM FEMBOYS"));
+console.log(textToMorse("Pourin bubbly, drank that muddy"));
+console.log(textToMorse("Future"));
+console.log(morseToText(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."));
+console.log(morseToText("-- -- -- / ..-. . -- -... --- -.-- ..."));
+console.log(morseToText(".--. --- ..- .-. .. -. / -... ..- -... -... .-.. -.-- --..-- / -.. .-. .- -. -.- / - .... .- - / -- ..- -.. -.. -.--"));
+console.log(morseToText("..-. ..- - ..- .-. ."));
---
+++ script.ts
@@ -0,0 +1,69 @@
+const morseAlphabet = [
+    ".-",  //A
+    "-...",  //B
+    "-.-.",  //C
+    "-..",  //D
+    ".",  //E
+    "..-.",  //F
+    "--.",  //G
+    "....",  //H
+    "..",  //I
+    ".---",  //J
+    "-.-",  //K
+    ".-..",  //L
+    "--",  //M
+    "-.",  //N
+    "---",  //O
+    ".--.",  //P
+    "--.-",  //Q
+    ".-.",  //R
+    "...",  //S
+    "-",  //T
+    "..-",  //U
+    "...-",  //V
+    ".--",  //W
+    "-..-",  //X
+    "-.--",  //Y
+    "--.."  //Z
+];
+
+function textToMorse(vstup: string): string {
+    let vystup:string = "";
+    vstup = vstup.toUpperCase();
+    for (let i:number = 0; i < vstup.length; i++) {
+        let kod:number = vstup.charCodeAt(i);
+        if (kod >= 65 && kod <= 90) { // A-Z
+            vystup += morseAlphabet[kod - 65] + " ";
+        } else if (kod === 32) { // space
+            vystup += "/ ";
+        }
+    }
+
+
+    return vystup;
+}
+
+function morseToText(vstup: string): string {
+    let vystup:string = "";
+    let morseChars:string[] = vstup.split(" "); 
+    for (let i:number = 0; i < morseChars.length; i++) {
+        if (morseChars[i] === "/") {
+            vystup += " ";
+        } else {
+            let index:number = morseAlphabet.indexOf(morseChars[i]);
+            if (index !== -1) {
+                vystup += String.fromCharCode(index + 65);
+            }
+        }
+    }
+    return vystup;
+}
+// Test cases
+console.log(textToMorse("HELLO WORLD"));
+console.log(textToMorse("MMM FEMBOYS"));
+console.log(textToMorse("Pourin bubbly, drank that muddy"));
+console.log(textToMorse("Future"));
+console.log(morseToText(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."));
+console.log(morseToText("-- -- -- / ..-. . -- -... --- -.-- ..."));
+console.log(morseToText(".--. --- ..- .-. .. -. / -... ..- -... -... .-.. -.-- --..-- / -.. .-. .- -. -.- / - .... .- - / -- ..- -.. -.. -.--"));
+console.log(morseToText("..-. ..- - ..- .-. ."));
